c3b20451930c5bf81a5918e04cd9867fd086ca8d
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-01-21 11:44:59 +0530

    Add files via upload

---
+++ Shutdown.cmd
@@ -0,0 +1,12 @@
+@echo off
+title Shutdown
+cls
+echo.
+echo.
+echo *******************************************************
+echo              ShutDown - GAK's XPS
+echo *******************************************************
+echo.
+
+pause
+shutdown /s /t 0
